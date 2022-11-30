0d7921586137a0a2728f9da93b4bdf47cdb4a6ab
aquaticpotato3677 <72111431+aquaticpotato3677@users.noreply.github.com>
2022-11-30 18:52:55 -0500

    initial commit

---
+++ process.js
@@ -0,0 +1,99 @@
+let fs = require('fs');
+const csv = require('csv-parse/sync');
+let agencies = ['berkshire-ma-us','brockton-ma-us','capeann-ma-us','capecod-ma-us','frta-ma-us','gatra-ma-us','lowell-ma-us','marthasvineyard-ma-us','merrimackvalley-ma-us','montachusett-ma-us','mwrta-ma-us','nantucket-ma-us','pvta-ma-us','srta-ma-us','wrta-ma-us'];
+let json = {
+    'sources':[],
+    'layers':[]
+};
+for(let i=0; i<agencies.length; i++){
+    let routes = csv.parse(fs.readFileSync('./data/'+agencies[i]+'/routes.txt').toString());
+    let trips = csv.parse(fs.readFileSync('./data/'+agencies[i]+'/trips.txt').toString());
+    let shapes = csv.parse(fs.readFileSync('./data/'+agencies[i]+'/shapes.txt').toString());
+    
+    let routesKey = routes[0];
+    let routeIdInd = null;
+    let routeShortNameInd = null;
+    let routeColorInd = null;
+    for(let j=0; j<routesKey.length; j++){
+        if(routesKey[j]=='route_id') routeIdInd = j;
+        if(routesKey[j]=='route_short_name') routeShortNameInd = j;
+        if(routesKey[j]=='route_color') routeColorInd = j;
+    }
+    for(let j=1; j<routes.length; j++){
+        let route = routes[j];
+        let routeId = route[routeIdInd];
+        let routeName = route[routeShortNameInd];
+        let routeColor = routeColorInd==null?getRandomColor():route[routeColorInd];
+        if(['', undefined, null].includes(routeColor)) routeColor = getRandomColor();
+        let shapeInd = null;
+        let routeInd = null;
+        let tripsKey = trips[0];
+        for(let k=0; k<tripsKey.length; k++){
+            if(tripsKey[k]=='route_id') routeInd = k;
+            if(tripsKey[k]=='shape_id') shapeInd = k;
+        }
+        // find all trips and shapes that match this route id
+        let routeShapes = new Map();
+        for(let k=1; k<trips.length; k++){
+            let trip = trips[k];
+            if(trip[routeInd]==routeId){
+                if(!routeShapes.has(trip[shapeInd])) routeShapes.set(trip[shapeInd], {'route':routeName,'coords':[]});
+            }
+        }
+
+        for(let k=1; k<shapes.length; k++){
+            let shape = shapes[k];
+            let shapeId = shape[0];
+            if(routeShapes.has(shapeId)){
+                routeShapes.get(shapeId).coords.push([shape[2],shape[1]]);
+            }
+        }
+        if(!routeColor.startsWith('#')) routeColor = '#'+routeColor;
+        let features = new Array();
+        for(let [key, value] of routeShapes){
+            let feature = {
+                'type':'Feature',
+                'properties':{},
+                'geometry':{
+                    'type':'LineString',
+                    'coordinates':value.coords
+                }
+            }
+            features.push(feature);
+        }
+        json.sources.push({
+            'id':agencies[i]+routeId,
+            'data':{
+                'type':'geojson',
+                'data':{
+                    'type':'FeatureCollection',
+                    'features':features
+                }
+            }
+        });
+        json.layers.push({
+            'id':agencies[i]+routeId,
+            'type':'line',
+            'source':agencies[i]+routeId,
+            'layout': {
+                'line-join': 'round',
+                'line-cap': 'round'
+            },
+            'paint':{
+                'line-color':routeColor,
+                'line-width':5
+            }
+        })
+    }
+}
+
+fs.writeFileSync('./data.json',JSON.stringify(json));
+
+function getRandomColor() {
+    let letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
