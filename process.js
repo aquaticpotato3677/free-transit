399c8fd5a756bd642ccfd35969242b4c4753fd9b
aquaticpotato3677 <72111431+aquaticpotato3677@users.noreply.github.com>
2022-12-01 16:41:49 -0500

    add hacky workarounds for bad gtfs data

--- process.js
+++ process.js
@@ -241,7 +241,7 @@ for(let i=0; i<agencies.length; i++){
     let routeLongNameInd = null;
     let routeUrlInd = null;
     for(let j=0; j<routesKey.length; j++){
-        if(routesKey[j]=='route_id') routeIdInd = j;
+        if(routesKey[j].trim()=='route_id') routeIdInd = j;
         if(routesKey[j]=='route_short_name') routeShortNameInd = j;
         if(routesKey[j]=='route_color') routeColorInd = j;
         if(routesKey[j]=='route_long_name') routeLongNameInd = j;
@@ -249,20 +249,20 @@ for(let i=0; i<agencies.length; i++){
     }
     for(let j=1; j<routes.length; j++){
         let route = routes[j];
-        let routeId = route[routeIdInd];
-        let routeName = route[routeShortNameInd];
+        let routeId = route[routeIdInd].replaceAll('"','');
+        let routeName = route[routeShortNameInd].replaceAll('"','');
         if(routeName=='') routeName = route[routeLongNameInd];
         let routeColor = routeColorInd==null?getRandomColor():route[routeColorInd];
         if(['', undefined, null].includes(routeColor)) routeColor = getRandomColor();
         if(!routeColor.startsWith('#')) routeColor = '#'+routeColor;
         let routeUrl = agencies[i].url;
-        if(routeUrlInd!=null) routeUrl = route[routeUrlInd];
+        if(routeUrlInd!=null) routeUrl = route[routeUrlInd].replaceAll('"','');
         if(routeUrl=='') routeUrl = agencies[i].url;
         let shapeInd = null;
         let routeInd = null;
         let tripsKey = trips[0];
         for(let k=0; k<tripsKey.length; k++){
-            if(tripsKey[k]=='route_id') routeInd = k;
+            if(tripsKey[k].trim()=='route_id') routeInd = k;
             if(tripsKey[k]=='shape_id') shapeInd = k;
         }
         // find all trips and shapes that match this route id
@@ -280,12 +280,11 @@ for(let i=0; i<agencies.length; i++){
         let shapeOrderInd = null;
         let shapesKey = shapes[0];
         for(let k=0; k<shapesKey.length; k++){
-            if(shapesKey[k]=='shape_id') shapeIdInd = k;
+            if(shapesKey[k].trim()=='shape_id') shapeIdInd = k;
             if(shapesKey[k]=='shape_pt_sequence') shapeOrderInd = k;
             if(shapesKey[k]=='shape_pt_lon') shapeLonInd = k;
             if(shapesKey[k]=='shape_pt_lat') shapeLatInd = k;
         }
-
         for(let k=1; k<shapes.length; k++){
             let shape = shapes[k];
             let shapeId = shape[shapeIdInd];
