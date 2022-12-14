48847c0cd9e660d86f463bff535adc64ed13755c
aquaticpotato3677 <72111431+aquaticpotato3677@users.noreply.github.com>
2022-12-14 14:58:19 -0500

    automate better

--- fetch.js
+++ fetch.js
@@ -4,20 +4,19 @@ let bent = require('bent');
 let getstream = bent(200, 302);
 let json = JSON.parse(fs.readFileSync('./agencies.json'));
 fs.rmdirSync('./data');
-async function a(){
+fs.mkdirSync('./data');
+async()=>{
     let wantedFiles = ['routes.txt','shapes.txt','trips.txt'];
-    fs.mkdirSync('./data');
     for(let i=0; i<json.length; i++){
         fs.mkdirSync('./data/'+json[i].loc);
-        let a = await getstream(json[i].gtfs);
-        a.pipe(unzipper.Parse()).on('entry',(entry)=>{
+        let data = await getstream(json[i].gtfs);
+        data.pipe(unzipper.Parse()).on('entry',(entry)=>{
             let fileName = entry.path;
             if(wantedFiles.includes(fileName)){
-                entry.pipe(fs.createWriteStream('data/'+json[i].loc+'/'+fileName));
+                entry.pipe(fs.createWriteStream('./data/'+json[i].loc+'/'+fileName));
             }else{
                 entry.autodrain();
             }
         });
     }
 }
-// a();
