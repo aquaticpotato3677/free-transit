f86f464b843c1793975878f46dbf240f722071db
aquaticpotato3677 <72111431+aquaticpotato3677@users.noreply.github.com>
2022-12-21 23:39:31 -0500

    clean fetch and more missoula testing

--- fetch.mjs
+++ fetch.mjs
@@ -3,24 +3,21 @@ import unzipper from 'unzipper';
 import fetch from 'node-fetch';
 let json = JSON.parse(fs.readFileSync('./agencies.json'));
 fs.mkdirSync('./gtfs');
-async function run(){
-    let wantedFiles = ['routes.txt','shapes.txt','trips.txt'];
-    for(let i=0; i<json.length; i++){
-        fs.mkdirSync('./gtfs/'+json[i].loc);
-        try{
-            let data = (await fetch(json[i].gtfs)).body;
-            data.pipe(unzipper.Parse()).on('entry',(entry)=>{
-                let fileName = entry.path;
-                if(wantedFiles.includes(fileName)){
-                    entry.pipe(fs.createWriteStream('./gtfs/'+json[i].loc+'/'+fileName));
-                }else{
-                    entry.autodrain();
-                }
-            });
-        }catch(e) {
-            console.log(e);
-            console.log(json[i].loc);
-        }
+let wantedFiles = ['routes.txt','shapes.txt','trips.txt'];
+for(let i=0; i<json.length; i++){
+    fs.mkdirSync('./gtfs/'+json[i].loc);
+    try{
+        let data = (await fetch(json[i].gtfs)).body;
+        data.pipe(unzipper.Parse()).on('entry',(entry)=>{
+            let fileName = entry.path;
+            if(wantedFiles.includes(fileName)){
+                entry.pipe(fs.createWriteStream('./gtfs/'+json[i].loc+'/'+fileName));
+            }else{
+                entry.autodrain();
+            }
+        });
+    }catch(e) {
+        console.log(e);
+        console.log(json[i].loc);
     }
 }
-run();
